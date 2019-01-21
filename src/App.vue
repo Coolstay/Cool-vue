5677cf3372ee39d60ba4d5f8f5e114fdb9ca706e
Cool <lvkstay@163.com>
2019-01-21 18:06:48 +0800

    创建了登陆页面

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- router view用于匹配路由 -->
     <router-view/>
   </div>
 </template>
---
+++ src/views/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="login">
+      <p>login页面</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
